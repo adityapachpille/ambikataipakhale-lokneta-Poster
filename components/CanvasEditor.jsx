f7ea74a4d034a65deb2a80732748408cde88abae
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2026-01-03 16:30:27 +0530

    Adjust image dimensions and title text

--- components/CanvasEditor.jsx
+++ components/CanvasEditor.jsx
@@ -9,13 +9,13 @@ export default function CanvasEditor() {
   const [offset, setOffset] = useState({ x: 0, y: 0 });
 
   const canvasSize = 600;
-  const [imageWidth, setImageWidth] = useState(155);
+  const [imageWidth, setImageWidth] = useState(157);
   const [imageHeight, setImageHeight] = useState(215);
   const [scale, setScale] = useState(1);
 
   const [position, setPosition] = useState({
-    x: 57,
-    y: canvasSize / 2 - imageHeight / 2 + 40,
+    x: 55,
+    y: canvasSize / 2 - imageHeight / 2 + 45,
   });
 
   const handleUpload = (e) => {
@@ -124,7 +124,7 @@ export default function CanvasEditor() {
     <>
       {/* Top bar */}
       <div className="topBarWrapper">
-        <h2 className="title">📲🗳️ Lokneta Poster Editor Softwareee 👥📸✨</h2>
+        <h2 className="title">📲🗳️ Lokneta Poster Editor Software 👥📸✨</h2>
         <div className="topBar">
           <button className="button" onClick={() => fileRef.current?.click()}>
             📤 Import Photo
