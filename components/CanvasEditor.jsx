d0b754cf7c7139d7eed93db1aab059cc9d59ca68
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2026-01-03 16:38:40 +0530

    Fix typo in title and adjust position value

--- components/CanvasEditor.jsx
+++ components/CanvasEditor.jsx
@@ -15,7 +15,7 @@ export default function CanvasEditor() {
 
   const [position, setPosition] = useState({
     x: 52,
-    y: canvasSize / 2 - imageHeight / 2 + 48,
+    y: canvasSize / 2 - imageHeight / 2 + 50,
   });
 
   const handleUpload = (e) => {
@@ -124,7 +124,7 @@ export default function CanvasEditor() {
     <>
       {/* Top bar */}
       <div className="topBarWrapper">
-        <h2 className="title">📲🗳️ Lokneta Poster Editor Software 👥📸✨</h2>
+        <h2 className="title">📲🗳️ Lokneta Poster Editor Softwaree 👥📸✨</h2>
         <div className="topBar">
           <button className="button" onClick={() => fileRef.current?.click()}>
             📤 Import Photo
